bf798c3a2f6563f84ed023ede2873fbc64553bd1
anbat <38318538+anbat@users.noreply.github.com>
2018-04-12 19:31:35 +0530

    Update scanline.c

--- scanline.c
+++ scanline.c
@@ -1,10 +1,7 @@
-#include<stdio.h>
 #include<GL/glut.h>
-//#include<stdlib.h>
-//#include<time.h>
-//#include<math.h>
+#include<stdio.h>
+
 float et[4][4]={{100,250,200,-1},{100,250,200,1},{200,150,300,1},{200,350,300,-1}};
-//float et[5][4]={{100,100,300,-0.25},{150,400,300,-2.33},{150,400,250,-2},{200,200,250,2},{100,100,200,1}};
 int np=4;
 float ae[4][3];
 float js;
@@ -15,141 +12,153 @@ static int menu_id;
 static int submenu_id;
 static int value=0;
 
-void addet()
+
+void addaet()
 {
-	int i;
-	for(i=0;i<np;i++)
-	{
-		printf("Scanline=%f and iaete=%d\n",js,iaet);
-		if(js==et[i][0])
-		{
-			ae[iaet][0]=et[i][1];
-			ae[iaet][1]=et[i][2];
-			ae[iaet][2]=et[i][3];
-			if(ae[iaet][1]>ymax)
-				ymax=ae[iaet][1];
-			iaet++;
-		}
-	}
+    int i;
+    for(i=0;i<np;i++)
+    {
+        printf("Scan line=%f & iate=%d\n",js,iaet);
+        if(js==et[i][0])
+        {
+         ae[iaet][0]=et[i][1];
+         ae[iaet][1]=et[i][2];
+         ae[iaet][2]=et[i][3];
+         if(ae[iaet][1]>ymax)
+            ymax=ae[iaet][1];
+         iaet++;
+        }
+    }
 }
 
 void upaet()
 {
-	int i;
-	for(i=0;i<np;i++)
-		ae[i][0]=ae[i][0]+ae[i][2];
+    int i;
+    for(i=0;i<np;i++)
+        ae[i][0]=ae[i][0]+ae[i][2];
 }
 
 void draw_pixel(float x1,float x2)
 {
-	int i;
-	float n;
-	for(n=x1;n<x2;n++)
-	{
-		glBegin(GL_POINTS);
-		glVertex2f(n,js);
-		glEnd();
-	}
-	glFlush();
-	printf("x1=%f x2=%f \n",x1,x2);
+    int i;
+    float n;
+
+    for(n=x1;n<=x2;n++)
+    {
+        glBegin(GL_POINTS);
+        glVertex2f(n,js);
+        glEnd();
+    }
+        glFlush();
+    printf("x1=%f x2=%f\n",x1,x2);
+}
+
+void drawText(float x, float y,float z,char* s){
+        int i;
+        glRasterPos2f(x,y);
+        for(i=0;s[i] != '\0';i++)
+                glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,s[i]);
 }
 
 void fill_poly()
 {
-	float x[3]={0,0,0};
-	int i=0,j;
-	do{
-		i=0;
-		addet();
-		printf("1=%f 2=%f %f\n",ae[0][1],ae[1][1],js);
-		for(j=0;j<np;j++)
-		{
-			if(ae[j][1]>js)
-			{
-				x[i]=ae[j][0];
-				i++;
-			}
-		}
-		draw_pixel(x[0],x[1]);
-		upaet();
-		js++;
-	}while(js<=ymax);
+    float x[3]={0,0,0};
+    int i=0,j;
+    do
+    {
+        i=0;
+        addaet();
+        printf("1=%f 2=%f %f\n",ae[0][1],ae[1][1],js);
+        for(j=0;j<np;j++)
+            if(ae[j][1]>js)
+            {
+                x[i]=ae[j][0];
+                i++;
+            }
+        draw_pixel(x[0],x[1]);
+        upaet();
+        js++;
+    }while(js <= ymax);
 }
 
 void empty_ae()
 {
-	js=et[0][0];
-	iaet=0;
+    js=et[0][0];
+    iaet=0;
 }
 
 void display()
 {
-	int i,j;
-	glClear(GL_COLOR_BUFFER_BIT);
-	
-	switch(value)
-	{
-		case 1: return;
-			break;
-		case 2: glColor3f(1.0,0.0,0.0);
-			fill_poly();
-			empty_ae();
-			break;
-		case 3: glColor3f(0.0,1.0,0.0);
-			fill_poly();
-			empty_ae();
-			break;
-		case 4: glColor3f(0.0,0.0,1.0);
-			fill_poly();
-			empty_ae();
-			break;
-	}
+    int i,j;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0,1.0,0.0);
+    drawText(200,200,0.5,"Scanline Filling Algorithm");
+        drawText(200,150,0.6,"NAME : PRAJWAL H P    USN : 1BI15CS116   BATCH : C-2");
+
+
+    switch(value)
+    {
+        case 1 : return;
+             break;
+        case 2 : glColor3f(1.0,0.0,0.0);
+             fill_poly();
+             empty_ae();
+             break;
+        case 3 : glColor3f(0.0,1.0,0.0);
+             fill_poly();
+             empty_ae();
+             break;
+        case 4 : glColor3f(0.0,0.0,1.0);
+             fill_poly();
+             empty_ae();
+             break;
+    }
 }
 
 void myinit()
 {
-	glClearColor(1.0,1.0,1.0,1.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-500.0,500.0,-500.0,500.0);
-	glMatrixMode(GL_MODELVIEW);
+    glClearColor(1.0,1.0,1.0,1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0,1000.0,0.0,1000.0);
 }
 
 void menu(int num)
 {
-	if(num==0)
-	{
-		glutDestroyWindow(window);
-		exit(0);
-	}
-	else
-		value=num;
+    if(num==0)
+    {
+        glutDestroyWindow(window);
+        exit(0);
+    }
+    else  value=num;
 }
 
 void createMenu(void)
 {
-	submenu_id=glutCreateMenu(menu);
-	glutAddMenuEntry("Red",2);
-	glutAddMenuEntry("Green",3);
-	glutAddMenuEntry("Blue",4);
-	menu_id=glutCreateMenu(menu);
-	glutAddMenuEntry("Clear",1);
-	glutAddSubMenu("Color",submenu_id);
-	glutAddMenuEntry("Quit",0);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
+    submenu_id=glutCreateMenu(menu);
+    glutAddMenuEntry("Red",2);
+    glutAddMenuEntry("Green",3);
+    glutAddMenuEntry("Blue",4);
+    menu_id=glutCreateMenu(menu);
+    glutAddMenuEntry("Clear",1);
+    glutAddSubMenu("Color",submenu_id);
+    glutAddMenuEntry("Quit",0);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
-int main(int argc,char** argv)
+
+int main(int argc,char ** argv)
 {
-	js=et[0][0];
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(500,500);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Polygon filling algorithm");
-	createMenu();
-	glutDisplayFunc(display);
-	myinit();
-	glutMainLoop();
-	return 0;
+    js=et[0][0];
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500,500);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Polygon Filling Algorithm");
+    createMenu();
+    glutDisplayFunc(display);
+    myinit();
+    glutMainLoop();
+    return 0;
 }
+
